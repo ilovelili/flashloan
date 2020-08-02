c186873d4d0368117a5a181122ebc1b7535a11bc
ilovelili <route666@live.cn>
2020-08-02 21:46:11 +0900

    add error handling

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -26,19 +26,24 @@ const wethAddress = addresses.tokens.weth;
 const soloAddress = addresses.dydx.solo;
 
 async function init() {
-  const networkId = await web3.eth.net.getId();
+  const networkId = await web3.eth.net.getId().catch((err) => console.error("failed to get network id"));
   const flashloan = new web3.eth.Contract(Flashloan.abi, Flashloan.networks[networkId].address);
   console.log(`NetworkId is ${networkId}`);
   // update eth price
   let ethPrice;
   const updateEthPrice = async () => {
-    const results = await kyber.methods.getExpectedRate("0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", daiAddress, 1).call();
+    const results = await kyber.methods
+      .getExpectedRate("0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", daiAddress, 1)
+      .call()
+      .catch((err) => console.error("failed to get eth price"));
+
     ethPrice = web3.utils.toBN("1").mul(web3.utils.toBN(results.expectedRate)).div(ONE_ETH_WEI);
     console.log(`eth price is ${ethPrice}`);
   };
   await updateEthPrice();
   setInterval(updateEthPrice, 15000);
 
+  // https://web3js.readthedocs.io/en/v1.2.7/web3-eth-subscribe.html
   web3.eth
     .subscribe("newBlockHeaders")
     .on("data", async (block) => {
@@ -47,8 +52,9 @@ async function init() {
       // uniswap uses weth
       const [dai, weth] = await Promise.all(
         [daiAddress, wethAddress].map((tokenAddress) => Token.fetchData(ChainId.MAINNET, tokenAddress))
-      );
-      const daiWeth = await Pair.fetchData(dai, weth);
+      ).catch((err) => console.error("failed to fetch uniswap token data"));
+
+      const daiWeth = await Pair.fetchData(dai, weth).catch((err) => console.error("failed to fetch uniswap pair data"));
 
       const AMOUNT_DAI_WEI = web3.utils.toBN(web3.utils.toWei(parseInt(AMOUNT_ETH * ethPrice).toString()));
       console.log(`Amount dai wei: ${AMOUNT_DAI_WEI}`);
@@ -57,7 +63,7 @@ async function init() {
       const amountsEth = await Promise.all([
         kyber.methods.getExpectedRate(daiAddress, "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", AMOUNT_DAI_WEI).call(),
         daiWeth.getOutputAmount(new TokenAmount(dai, AMOUNT_DAI_WEI)),
-      ]);
+      ]).catch((err) => console.error("failed to get eth expected rate"));
 
       const ethFromKyber = AMOUNT_DAI_WEI.mul(web3.utils.toBN(amountsEth[0].expectedRate)).div(ONE_ETH_WEI);
       const ethFromUniswap = web3.utils.toBN(amountsEth[1][0].raw.toString());
@@ -66,7 +72,8 @@ async function init() {
       const amountsDai = await Promise.all([
         kyber.methods.getExpectedRate("0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", daiAddress, ethFromUniswap.toString()).call(),
         daiWeth.getOutputAmount(new TokenAmount(weth, ethFromKyber.toString())),
-      ]);
+      ]).catch((err) => console.error("failed to get dai expected rate"));
+
       const daiFromKyber = ethFromUniswap.mul(web3.utils.toBN(amountsDai[0].expectedRate)).div(ONE_ETH_WEI);
       const daiFromUniswap = web3.utils.toBN(amountsDai[1][0].raw.toString());
       console.log(`Kyber -> Uniswap. Dai input / output: ${AMOUNT_DAI_WEI.toString()} / ${daiFromUniswap.toString()}`);
@@ -74,7 +81,9 @@ async function init() {
 
       if (daiFromUniswap.gt(AMOUNT_DAI_WEI)) {
         const tx = flashloan.methods.initateFlashLoan(soloAddress, daiAddress, AMOUNT_DAI_WEI, DIRECTION.KYBER_TO_UNISWAP);
-        const [gasPrice, gasCost] = await Promise.all([web3.eth.getGasPrice(), tx.estimateGas({from: admin})]);
+        const [gasPrice, gasCost] = await Promise.all([web3.eth.getGasPrice(), tx.estimateGas({from: admin})]).catch((err) =>
+          console.error("failed to get gas price")
+        );
 
         const txCost = web3.utils.toBN(gasCost).mul(web3.utils.toBN(gasPrice)).mul(ethPrice);
         const profit = daiFromUniswap.sub(AMOUNT_DAI_WEI).sub(txCost);
@@ -90,7 +99,7 @@ async function init() {
             gas: gasCost,
             gasPrice,
           };
-          const receipt = await web3.eth.sendTransaction(txData);
+          const receipt = await web3.eth.sendTransaction(txData).catch((err) => console.error("failed to send transaction"));
           console.log(`Transaction hash: ${receipt.transactionHash}`);
 
           const record = `${new Date().getTime()}: Kyber -> Uniswap | Tx hash: ${receipt.transactionHash}\n`;
@@ -102,7 +111,9 @@ async function init() {
 
       if (daiFromKyber.gt(AMOUNT_DAI_WEI)) {
         const tx = flashloan.methods.initateFlashLoan(soloAddress, daiAddress, AMOUNT_DAI_WEI, DIRECTION.UNISWAP_TO_KYBER);
-        const [gasPrice, gasCost] = await Promise.all([web3.eth.getGasPrice(), tx.estimateGas({from: admin})]);
+        const [gasPrice, gasCost] = await Promise.all([web3.eth.getGasPrice(), tx.estimateGas({from: admin})]).catch((err) =>
+          console.error("failed to get gas price")
+        );
 
         const txCost = web3.utils.toBN(gasCost).mul(web3.utils.toBN(gasPrice)).mul(ethPrice);
         const profit = daiFromKyber.sub(AMOUNT_DAI_WEI).sub(txCost);
@@ -119,7 +130,7 @@ async function init() {
             gas: gasCost,
             gasPrice,
           };
-          const receipt = await web3.eth.sendTransaction(txData);
+          const receipt = await web3.eth.sendTransaction(txData).catch((err) => console.error("failed to send transaction"));
           console.log(`Transaction hash: ${receipt.transactionHash}`);
 
           const record = `${new Date().getTime()}: Uniswap -> Kyber | Tx hash: ${receipt.transactionHash}\n`;
@@ -135,7 +146,7 @@ async function init() {
 }
 
 async function logChainEvent() {
-  const networkId = await web3.eth.net.getId();
+  const networkId = await web3.eth.net.getId().catch((err) => console.error("failed to get network id"));
   const flashloan = new web3.eth.Contract(Flashloan.abi, Flashloan.networks[networkId].address);
   // log event
   flashloan.events
@@ -153,7 +164,7 @@ async function logChainEvent() {
       });
     })
     .on("error", (err) => {
-      // console.error(err);
+      console.error(err);
     });
 }
 
